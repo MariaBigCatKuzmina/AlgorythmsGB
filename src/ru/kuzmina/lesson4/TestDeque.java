78b2d36a3586bed84d8c8e9837b5c33b7c37a1bb
Maria Kuzmina <bigcat82@gmail.com>
2022-04-11 21:27:43 +0300

    Homework4

---
+++ src/ru/kuzmina/lesson4/TestDeque.java
@@ -0,0 +1,25 @@
+package ru.kuzmina.lesson4;
+
+public class TestDeque {
+    public static void main(String[] args) {
+        DequeImpl<Integer> deque = new DequeImpl<>(5);
+        deque.insertLeft(1);
+        System.out.println(deque);
+        deque.insertLeft(2);
+        System.out.println(deque);
+
+        deque.insertRight(3);
+        System.out.println(deque);
+        deque.insertRight(4);
+        System.out.println(deque);
+
+        deque.insertLeft(5);
+        System.out.println(deque);
+
+        deque.insertLeft(6);
+        System.out.println(deque);
+
+        deque.removeLeft();
+        System.out.println(deque);
+    }
+}
